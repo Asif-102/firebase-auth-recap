43ce5f99367361b41b0e173de5f038ec179eafe2
Asif2535 <aiqbal517@gmail.com>
2021-03-15 01:32:30 +0600

    authentication process learned with google and facebook

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBV3FcklNx7rUr3V-bw2pQ6MXHSgf9Jd-g",
+    authDomain: "fir-auth-recap-ae011.firebaseapp.com",
+    projectId: "fir-auth-recap-ae011",
+    storageBucket: "fir-auth-recap-ae011.appspot.com",
+    messagingSenderId: "860176654660",
+    appId: "1:860176654660:web:c0ef95054c0cbe666c3711"
+  };
+
+  export default firebaseConfig;
